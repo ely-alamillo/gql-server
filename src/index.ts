9a8003f885cb3945a657f8d37046ec085cfbce75
Ely Alamillo <eleazar.alamillo@valic.com>
2019-01-23 14:51:14 -0600

    Configure redis, cors, session

--- src/index.ts
+++ src/index.ts
@@ -3,22 +3,56 @@ import { ApolloServer } from "apollo-server-express";
 import Express from "express";
 import { buildSchema, formatArgumentValidationError } from "type-graphql";
 import { createConnection } from "typeorm";
+import session from "express-session";
+import connectRedis from "connect-redis";
+import { redis } from "./redis";
+import cors from "cors";
 
 import { RegisterResolver } from "./modules/user/Register";
+import { LoginResolver } from "./modules/user/Login";
+import { MeResolver } from "./modules/user/me";
 
 const main = async () => {
   await createConnection();
 
   const schema = await buildSchema({
-    resolvers: [RegisterResolver]
+    resolvers: [RegisterResolver, LoginResolver, MeResolver]
   });
 
   const apolloServer = new ApolloServer({
     schema,
+    context: ({ req }: any) => ({ req }),
     formatError: formatArgumentValidationError
   });
 
   const app = Express();
+  const RedisStore = connectRedis(session);
+
+  // middleware
+
+  app.use(
+    cors({
+      origin: "http://localhost:4000",
+      credentials: true
+    })
+  );
+
+  app.use(
+    session({
+      store: new RedisStore({
+        client: redis as any
+      }),
+      name: "uid",
+      secret: "this is a secret",
+      resave: false,
+      saveUninitialized: true,
+      cookie: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 1000 * 60 * 60 * 24 * 7 * 365
+      }
+    })
+  );
 
   apolloServer.applyMiddleware({ app });
 
