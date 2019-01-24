5fea3e08fc5047e7cc2bca95ccc5d4bc8dcbee40
Ely Alamillo <eleazar.alamillo@valic.com>
2019-01-24 12:54:12 -0600

    add function to create confirmation link

---
+++ src/utils/createConfirmationUrl.ts
@@ -0,0 +1,9 @@
+import { v4 } from "uuid";
+import { redis } from "../redis";
+
+export const createConfirmationUrl = async (userId: number) => {
+  const id = v4();
+  await redis.set(id, userId, "ex", 60 * 60 * 24);
+
+  return `http://localhost:3000/user/confirm/${id}`;
+};
