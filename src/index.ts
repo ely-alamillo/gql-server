0a86f7b21bbd9ee6f478d8af83e1d3bf31eaeed9
Ely Alamillo <eleazar.alamillo@valic.com>
2019-01-24 12:56:00 -0600

    wired up new ConfirmUserResolver

--- src/index.ts
+++ src/index.ts
@@ -12,12 +12,18 @@ import { RegisterResolver } from "./modules/user/Register";
 import { LoginResolver } from "./modules/user/Login";
 import { MeResolver } from "./modules/user/me";
 import { customAuthChecker } from "./authChecker";
+import { ConfirmUserResolver } from "./modules/user/register/confirmUser";
 
 const main = async () => {
   await createConnection();
 
   const schema = await buildSchema({
-    resolvers: [RegisterResolver, LoginResolver, MeResolver],
+    resolvers: [
+      RegisterResolver,
+      LoginResolver,
+      MeResolver,
+      ConfirmUserResolver
+    ],
     authChecker: customAuthChecker
   });
 
@@ -63,5 +69,4 @@ const main = async () => {
   });
 };
 
-console.log("calling main");
 main();
