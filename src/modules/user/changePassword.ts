55d10150fa17cd9a9457259d1f0ea49106a0d345
Ely Alamillo <ely0013@gmail.com>
2019-01-24 20:32:30 -0600

    add change password resolver

--- src/modules/user/changePassword.ts
+++ src/modules/user/changePassword.ts
@@ -1,18 +1,19 @@
-import { Resolver, Mutation, Arg } from "type-graphql";
+import { Resolver, Mutation, Arg, Ctx } from "type-graphql";
 import bcrypt from "bcryptjs";
 import { redis } from "../../redis";
 import { User } from "../../entity/User";
 import { forgotPasswordPrefix } from "../../constants/redisPrefixes";
 import { ChangePasswordInput } from "./changePassword/ChangePasswordInput";
+import { MyContext } from "../../types/MyContext";
 
 @Resolver()
 export class ChangePasswordResolver {
   @Mutation(() => User, { nullable: true })
-  async changePassword(@Arg("data")
-  {
-    token,
-    password
-  }: ChangePasswordInput): Promise<User | null> {
+  async changePassword(
+    @Arg("data")
+    { token, password }: ChangePasswordInput,
+    @Ctx() ctx: MyContext
+  ): Promise<User | null> {
     const userId = await redis.get(forgotPasswordPrefix + token);
 
     if (!userId) {
@@ -29,7 +30,10 @@ export class ChangePasswordResolver {
     redis.del(forgotPasswordPrefix + token);
     // we can also do user.update
     user.password = await bcrypt.hash(password, 12);
-    user.save();
+    await user.save();
+
+    // log use back in after changed pass
+    ctx.req.session!.userId = user.id;
 
     return user;
   }
